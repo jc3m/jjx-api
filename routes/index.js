35d41d0378493a82e5645508a8e1bb3f2537f39d
Jonathan Xu <jonathanj.xu97@gmail.com>
2017-09-05 21:43:10 -0500

    Baful commit

--- routes/index.js
+++ routes/index.js
@@ -6,5 +6,22 @@ router.get('/', (req, res, next) => {
   res.json({ success: true, message: 'root' });
 });
 
+router.get('/iamrahul', (req, res, next) => {
+  res.json({
+    success: true,
+    color: 'brown',
+    dish: 'Chicken Tikka Misala',
+    restaurant: 'Taste of Surti',
+    address: {
+      street: 'Bhangra Ave',
+      city: 'Browntown',
+      Country: 'USA'
+    },
+    height: 4.2,
+    netWorth: 6.9,
+    status: 'cuck'
+  });
+});
+
 module.exports = router;
 
